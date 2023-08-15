6430af3620cb58129264548ed8ebfcd39471375e
David V <david.v@scrapehero.com>
2023-08-15 19:57:43 +0530

    Added doc string

--- crawl_executor/utils/url_queue.py
+++ crawl_executor/utils/url_queue.py
@@ -7,10 +7,13 @@ class UrlQueue:
         self.queue_name = queue_name
 
     def get(self):
+        """Fetch a URL from queue"""
         return self.redis_client.rpop(self.queue_name)
 
     def push(self, url):
+        """Push a URL from queue"""
         self.redis_client.lpush(self.queue_name, url)
 
     def is_queue_empty(self):
+        """Check if URL queue is empty"""
         return self.redis_client.llen(self.queue_name) == 0
