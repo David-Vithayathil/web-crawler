24b0622af23231681fd535326208c58a2b50528a
David V <david.v@scrapehero.com>
2023-08-16 01:41:14 +0530

    Fixed lint

--- crawl_executor/main.py
+++ crawl_executor/main.py
@@ -7,6 +7,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
